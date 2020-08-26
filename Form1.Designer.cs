f9ea2019649e13ef4f8826c43b44bc0b2346c243
wangsc <645708679@qq.com>
2020-08-26 15:24:12 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FundMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelFundIncrease = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.BackColor = System.Drawing.Color.Transparent;
+            this.labelPrice.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelPrice.ForeColor = System.Drawing.Color.White;
+            this.labelPrice.Location = new System.Drawing.Point(12, 51);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(100, 17);
+            this.labelPrice.TabIndex = 0;
+            this.labelPrice.Text = "0";
+            this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelFundIncrease
+            // 
+            this.labelFundIncrease.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFundIncrease.BackColor = System.Drawing.Color.Transparent;
+            this.labelFundIncrease.Font = new System.Drawing.Font("方正粗黑宋简体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelFundIncrease.ForeColor = System.Drawing.Color.White;
+            this.labelFundIncrease.Location = new System.Drawing.Point(12, 9);
+            this.labelFundIncrease.Name = "labelFundIncrease";
+            this.labelFundIncrease.Size = new System.Drawing.Size(202, 42);
+            this.labelFundIncrease.TabIndex = 0;
+            this.labelFundIncrease.Text = "0";
+            this.labelFundIncrease.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelFundIncrease.MouseClick += new System.Windows.Forms.MouseEventHandler(this.labelFunIncrease_MouseClick);
+            this.labelFundIncrease.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelFunIncrease_MouseDown);
+            this.labelFundIncrease.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labelFunIncrease_MouseMove);
+            this.labelFundIncrease.MouseUp += new System.Windows.Forms.MouseEventHandler(this.labelFunIncrease_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTime
+            // 
+            this.labelTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTime.BackColor = System.Drawing.Color.Transparent;
+            this.labelTime.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTime.ForeColor = System.Drawing.Color.White;
+            this.labelTime.Location = new System.Drawing.Point(134, 51);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(80, 17);
+            this.labelTime.TabIndex = 1;
+            this.labelTime.Text = "00:00:00";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.ClientSize = new System.Drawing.Size(226, 77);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelFundIncrease);
+            this.Controls.Add(this.labelPrice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label labelFundIncrease;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
